91aeb953268e12202ab765fa6cb4ab36182e2a86
SB- Alumno <SB-Alumno@duoc.acad>
2026-03-25 13:35:33 -0300

    Proyecto completo

---
+++ src/test/java/com/example/CsrPrestamo/CsrPrestamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CsrPrestamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsrPrestamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
